a8646296f948edcb98244714210c1cf20fef6699
Kent <kspenc05@uoguelph.ca>
2018-10-22 19:33:07 -0400

    I MADE A CHEST OPEN

--- Assets/scripts/ChestScript.cs
+++ Assets/scripts/ChestScript.cs
@@ -7,18 +7,31 @@ public class ChestScript : MonoBehaviour {
 	public Sprite closed;
 	public Sprite open;
 	public GameObject chest;
-	public bool opened;
+    public bool opened;
+    public SpriteRenderer render;
+    Rigidbody2D rb; 
 
 	void Start()
 	{
-			SpriteRenderer renderer = chest.AddComponent<SpriteRenderer>();
-			renderer.sortingOrder = 1;
+			render = chest.AddComponent<SpriteRenderer>();
+            render.sortingOrder = 1;
+    
+            rb = chest.AddComponent<Rigidbody2D>();
+            rb.bodyType = RigidbodyType2D.Static;
 
-			if(opened) {
-				renderer.sprite = open;
-			}
-			else {
-				renderer.sprite = closed;
-			}
+            BoxCollider2D collider = chest.AddComponent<BoxCollider2D>();
+            collider.size = new Vector2(0.05f, 0.07f);
+
+            render.sprite = closed;
+            opened = false;
 	}
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if(c.gameObject.CompareTag("Player"))
+        {
+            opened = true;
+            render.sprite = open;
+        }
+    }
 }
