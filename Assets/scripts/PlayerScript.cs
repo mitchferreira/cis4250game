2825c1b0cf2383a7e48ab69fc0a559795bd5b601
mitchferreira <mitchellferreira74@gmail.com>
2018-10-23 18:59:42 -0400

    Save and load chests and player data

---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public int level;
+	public int expPoints;
+	public float xCoordinate;
+	public float yCoordinate;
+	public GameObject player;
+
+	public void UpdatePlayerState(float x, float y, int lvl, int exp) {
+		level = lvl;
+		expPoints = exp;
+		xCoordinate = x;
+		yCoordinate = y;
+		UpdatePlayer();
+	}
+
+	void UpdatePlayer() {
+		player.transform.position = new Vector3(xCoordinate, yCoordinate, 0.0f);
+	}
+}
