6f62a96bb90a5a4c40f5390e589b0fa6d2caf50e
Joseph Chan <jchan24@uoguelph.ca>
2018-10-23 21:22:19 -0400

    adding framework for battle ui

---
+++ Assets/scripts/exitPanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exitPanel : MonoBehaviour {
+	public GameObject attack_panel;
+	
+	void hide_panel(){
+		attack_panel.SetActive(false);
+	}
+}
