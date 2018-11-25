c95c36bc53610a1526f0de2377bf4c02b5e0faf2
fiercescott <colliers136@gmail.com>
2018-11-25 13:16:15 -0500

    Add files via upload
    hopefully the final battle system

---
+++ BattleSystem3.5/EnemySelector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySelector : MonoBehaviour
+{
+
+
+
+
+    public void attackEnemy1()
+    {
+
+        Battle.enemySelector = 1;
+        //print(Battle.playerAttack);
+
+    }
+
+    public void attackEnemy2()
+    {
+
+        Battle.enemySelector = 2;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+
+}
